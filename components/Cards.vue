e8af34f3bc1641cd448c25fdf106d73d1deeecb0
Dmitrii Romanov <RomDim197@gmail.com>
2024-11-28 02:53:56 +0300

    wip cards

--- components/Cards.vue
+++ components/Cards.vue
@@ -32,15 +32,26 @@ defineOptions({
 <style lang="scss" scoped>
 .cards {
   display: grid;
-  grid-template-columns: 1fr 1fr 1fr;
-  grid-template-rows: 1fr 1fr 1fr;
+  grid-template-columns: repeat(3, 1fr);
+  grid-template-rows: repeat(3, 1fr);
+  width: 100%;
+  gap: 0;
+
+  .card {
+    width: 100%;
+    display: flex;
+  }
 
   .card__block {
+    padding: 10px;
+    width: 100%;
+    aspect-ratio: calc(340 / 240);
     display: flex;
     flex-direction: column;
     justify-content: space-between;
-    padding: 20px;
-    height: 100%;
+    align-items: flex-start;
+    position: relative;
+    overflow: hidden;
     background-size: cover;
     background-position: center;
 
@@ -64,6 +75,39 @@ defineOptions({
     }
     &.card-img-7 {
       background-image: url('/images/Card7.png');
+    }
+
+    .card__content {
+      display: flex;
+      flex-direction: column; /* Содержимое располагается вертикально */
+      justify-content: space-between; /* Равномерное распределение заголовков и кнопки */
+      align-items: center; /* Центровка по горизонтали */
+      text-align: center; /* Выравнивание текста */
+      color: white; /* Цвет текста */
+      padding: 10px; /* Отступы внутри карточки */
+      height: 100%; /* Растянуть содержимое на всю высоту */
+      width: 100%; /* На всю ширину */
+      background: rgba(0, 0, 0, 0.4); /* Прозрачный фон для читабельности текста */
+    }
+
+    h2,
+    h3 {
+      margin: 0;
+    }
+
+    button {
+      margin-top: 10px;
+      padding: 10px 15px;
+      border: none;
+      background-color: white;
+      color: black;
+      font-weight: bold;
+      cursor: pointer;
+      transition: transform 0.3s;
+    }
+
+    button:hover {
+      transform: scale(1.1);
     }
   }
 }
