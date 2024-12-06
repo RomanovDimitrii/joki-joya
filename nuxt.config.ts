f973699ea1ccc05b6e915a0deb7372f6b1cd9a3b
Dmitrii Romanov <RomDim197@gmail.com>
2024-12-06 04:34:17 +0300

    adaptive added

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,41 +1,17 @@
 export default defineNuxtConfig({
   app: {
+    baseURL: process.env.NODE_ENV === 'production' ? '/joki-joya/' : '/',
+    cdnURL: process.env.NODE_ENV === 'production' ? '/joki-joya/' : '/',
     head: {
-      title: 'Joki-Joya', // Устанавливает заголовок по умолчанию
+      title: 'Joki-Joya',
       meta: [{ name: 'description', content: 'Парки развлечений для детей' }],
       link: [
-        {
-          rel: 'icon',
-          type: 'image/x-icon',
-          href: '/favicon.ico'
-        },
-        {
-          rel: 'apple-touch-icon',
-          sizes: '180x180',
-          href: '/apple-touch-icon.png'
-        },
-        {
-          rel: 'icon',
-          type: 'image/png',
-          sizes: '96x96',
-          href: '/favicon-96x96.png'
-        },
-        {
-          rel: 'icon',
-          type: 'image/png',
-          sizes: '192x192',
-          href: '/web-app-manifest-192x192.png'
-        },
-        {
-          rel: 'icon',
-          type: 'image/png',
-          sizes: '512x512',
-          href: '/web-app-manifest-512x512.png'
-        },
-        {
-          rel: 'manifest',
-          href: '/site.webmanifest'
-        }
+        { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+        { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png' },
+        { rel: 'icon', type: 'image/png', sizes: '96x96', href: '/favicon-96x96.png' },
+        { rel: 'icon', type: 'image/png', sizes: '192x192', href: '/web-app-manifest-192x192.png' },
+        { rel: 'icon', type: 'image/png', sizes: '512x512', href: '/web-app-manifest-512x512.png' },
+        { rel: 'manifest', href: '/site.webmanifest' }
       ]
     }
   },
@@ -48,9 +24,9 @@ export default defineNuxtConfig({
   build: {
     loaders: {
       scss: {
-        implementation: require('sass') // Подключение реализации Sass
+        implementation: require('sass')
       }
     },
-    transpile: ['swiper'] // Транспиляция Swiper
+    transpile: ['swiper']
   }
 });
