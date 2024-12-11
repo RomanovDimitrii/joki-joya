3b079530fee0084a4b7f3cd6598d204d09e0c8bb
Dmitrii Romanov <RomDim197@gmail.com>
2024-12-12 01:26:38 +0300

    deploy links fx

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,21 +1,50 @@
 export default defineNuxtConfig({
   app: {
-    baseURL: process.env.NUXT_APP_BASE_URL || '/',
-    buildAssetsDir: '/_nuxt/' // Папка для статических файлов
+    baseURL: process.env.NUXT_APP_BASE_URL || '/', // Базовый путь для разработки и деплоя
+    head: {
+      link: [
+        {
+          rel: 'manifest',
+          href: `${process.env.NUXT_APP_BASE_URL || '/'}site.webmanifest` // Ссылка на манифест
+        },
+        {
+          rel: 'icon',
+          type: 'image/png',
+          sizes: '192x192',
+          href: `${process.env.NUXT_APP_BASE_URL || '/'}web-app-manifest-192x192.png`
+        },
+        {
+          rel: 'icon',
+          type: 'image/png',
+          sizes: '512x512',
+          href: `${process.env.NUXT_APP_BASE_URL || '/'}web-app-manifest-512x512.png`
+        }
+      ],
+      meta: [
+        {
+          name: 'theme-color',
+          content: '#ffffff' // Цвет темы
+        },
+        {
+          name: 'background-color',
+          content: '#ffffff' // Фоновый цвет
+        }
+      ]
+    }
   },
-  ssr: false, // Отключаем серверный рендеринг (SPA)
-  css: ['~/assets/styles/main.scss'], // Подключаем глобальные стили
+  ssr: false,
+  css: ['~/assets/styles/main.scss'],
   devtools: {
     enabled: true
   },
   nitro: {
-    preset: 'github-pages' // Указываем предустановку для деплоя на GitHub Pages
+    preset: 'github-pages'
   },
   build: {
-    transpile: ['swiper'], // Транспилируем Swiper для совместимости
+    transpile: ['swiper'],
     loaders: {
       scss: {
-        implementation: require('sass') // Используем Sass для SCSS файлов
+        implementation: require('sass')
       }
     }
   }
